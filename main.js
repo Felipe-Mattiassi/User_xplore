10ebd60df547b0d968f7c439104c6be0960c4810
Felipe Mattiassi <fepazmattiassi@gmail.com>
2024-08-15 17:47:32 -0300

    redireciona

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 
+const headerLinks = document.querySelectorAll('.button') 
 
+function getDistanceFromTheTop (element) {
+    const id = element.getAttribute("href")
+    const section = document.querySelector(id)
+    return document.querySelector(id).offsetTop
+}
+
+function nativeScroll(distanceFromTheTop) {
+    window.scroll({
+        top: distanceFromTheTop, 
+        behavior:"smooth",
+    })
+}
+
+
+function scrollToSection(e) {
+    e.preventDefault()
+    const element = e.target
+    const distanceFromTheTop = getDistanceFromTheTop(e.target)
+    nativeScroll(distanceFromTheTop)
+
+}
+
+headerLinks.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+})
